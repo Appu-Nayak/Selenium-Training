98f774584535e9719bf8a77efaafaca8f48b847b
Appu Nayak <167209962+Appu-Nayak@users.noreply.github.com>
2026-09-10 10:11:01 +0530

    Modifed the date Web Element
    Updated date selection logic in the date picker application.

--- Day-3/Day3_assnmt2.java
+++ Day-3/Day3_assnmt2.java
@@ -16,12 +16,12 @@ public class Day3_assnmt2 {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://demoapps.qspiders.com/ui/datePick?sublist=0");
-		driver.findElement(By.cssSelector("[type='text']")).click();
-		driver.findElement(By.cssSelector("[type='button']")).click();
-		driver.findElement(By.cssSelector("[role='option']")).click();
-		WebElement result=  driver.findElement(By.cssSelector("[type='text']"));
+		driver.findElement(By.cssSelector("[type='text']")).click(); //click on calender
+		driver.findElement(By.cssSelector("[aria-label='Next Month']")).click(); //For selecting next month
+		driver.findElement(By.cssSelector("[aria-label='Choose Tuesday, October 6th, 2026']")).click(); //For selecting date
+		WebElement result=  driver.findElement(By.cssSelector("[type='text']"));  
 		result.getDomAttribute("value");
-		if(result.getDomAttribute("value").contains("26/07/2026")) {
+		if(result.getDomAttribute("value").contains("06/10/2026")) {
 			System.out.println("Date is displayed Correctly");
 			
 		}else {
