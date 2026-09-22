408d29b2b80a6bbae46baad4398b442232351512
Appu Nayak <167209962+Appu-Nayak@users.noreply.github.com>
2026-09-22 19:02:52 +0530

    Add files via upload

---
+++ LastDayTask2/OrangeHrmBuzzTest.java
@@ -0,0 +1,24 @@
+package Assessments.LastDayTask2;
+
+import Assessments.LastDayTask2.utilities.ExcelUtility;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class OrangeHrmBuzzTest extends BaseClass{
+    @Test
+    public void buzzTest() throws IOException, InterruptedException {
+        String mindText= ExcelUtility.getData(1,0);
+        buzzPage.getBuzz();
+        Thread.sleep(2000);
+
+        buzzPage.getMindText(mindText);
+        buzzPage.getPost();
+        Thread.sleep(2000);
+
+        if(buzzPage.verifyRecentPost(mindText))
+            System.out.println("Post is displayed in Recent Posts");
+        else
+            System.out.println("Post is NOT displayed in Recent Posts");
+    }
+}
